68eae89fd797d62e11de72dfc5d528b0b9154c49
MsVoxxie <odysseylove222@gmail.com>
2023-03-27 19:12:04 +0000

    Goodbye!

--- eventsMusic/musicBot/empty.js
+++ eventsMusic/musicBot/empty.js
@@ -6,13 +6,15 @@ module.exports = {
 	async execute(channel, client) {
 		const settings = await client.getGuild(channel.guild);
 
+		const attachment = new AttachmentBuilder(`${join(__dirname, '../../images/vi/Goodbye.gif')}`, { name: 'Goodbye.gif' });
 		const embed = new EmbedBuilder()
 			.setColor(settings.guildColorHex)
 			.setTitle('**Channel Empty**')
-			.setDescription(`Voice channel is empty. Goodbye!`);
+			.setDescription(`Voice channel is empty. Goodbye!`)
+			.setThumbnail('attachment://Goodbye.gif');
 
-		channel.send({ embeds: [embed] }).then((m) => {
-			setTimeout(() => m.delete(), 60 * 1000);
+		channel.send({ embeds: [embed], files: [attachment] }).then((m) => {
+			setTimeout(() => m.delete(), 120 * 1000);
 		});
 	},
 };
--- eventsMusic/musicBot/finish.js
+++ eventsMusic/musicBot/finish.js
@@ -1,4 +1,5 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const { join } = require('path');
 
 module.exports = {
 	name: 'finish',
@@ -7,10 +8,15 @@ module.exports = {
 		const settings = await client.getGuild(queue.textChannel.guild);
 		if (queue.lastPlaying) await queue.lastPlaying.delete(); // Delete last playing if there is one, reduces spam.
 
-		const embed = new EmbedBuilder().setColor(settings.guildColorHex).setTitle('**Queue Empty**').setDescription(`Queue is empty. Goodbye!`);
+		const attachment = new AttachmentBuilder(`${join(__dirname, '../../images/vi/Goodbye.gif')}`, { name: 'Goodbye.gif' });
+		const embed = new EmbedBuilder()
+			.setColor(settings.guildColorHex)
+			.setTitle('**Queue Empty**')
+			.setDescription(`Queue is empty. Goodbye!`)
+			.setThumbnail('attachment://Goodbye.gif');
 
-		queue.textChannel.send({ embeds: [embed] }).then((m) => {
-			setTimeout(() => m.delete(), 60 * 1000);
+		queue.textChannel.send({ embeds: [embed], files: [attachment] }).then((m) => {
+			setTimeout(() => m.delete(), 120 * 1000);
 		});
 	},
 };
