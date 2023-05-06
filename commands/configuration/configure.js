f9ec327f7becbab23933c82f8f52b5c16c5487ab
MsVoxxie <odysseylove222@gmail.com>
2023-05-06 00:41:46 +0000

    yeah

--- commands/configuration/configure.js
+++ commands/configuration/configure.js
@@ -7,69 +7,110 @@ module.exports = {
 		.setDescription('Configure the bot for your server')
 		.setDefaultMemberPermissions(PermissionsBitField.ManageGuild)
 		.setDMPermission(false)
-		.addSubcommand((subCommand) =>
-			subCommand
+		.addSubcommandGroup((subGroup) =>
+			subGroup
 				.setName('auditlog')
-				.setDescription('Set the audit log channel')
-				.addChannelOption((option) => option.setName('channel').setDescription('The channel to set the audit log to').setRequired(true))
+				.setDescription('Configure the audit log channel')
+				.addSubcommand((subCommand) =>
+					subCommand
+						.setName('setchannel')
+						.setDescription('Set the audit log channel')
+						.addChannelOption((option) => option.setName('channel').setDescription('The channel to set the audit log to').setRequired(true))
+				)
+				.addSubcommand((subCommand) => subCommand.setName('removechannel').setDescription('Remove the audit log channel'))
 		)
-		.addSubcommand((subCommand) =>
-			subCommand
+		.addSubcommandGroup((subGroup) =>
+			subGroup
 				.setName('modlog')
-				.setDescription('Set the mod log channel')
-				.addChannelOption((option) => option.setName('channel').setDescription('The channel to set the mod log to').setRequired(true))
+				.setDescription('Configure the mod log channel')
+				.addSubcommand((subCommand) =>
+					subCommand
+						.setName('setchannel')
+						.setDescription('Set the mod log channel')
+						.addChannelOption((option) => option.setName('channel').setDescription('The channel to set the mod log to').setRequired(true))
+				)
+				.addSubcommand((subCommand) => subCommand.setName('removechannel').setDescription('Remove the mod log channel'))
 		)
-		.addSubcommand((subCommand) =>
-			subCommand
+		.addSubcommandGroup((subGroup) =>
+			subGroup
 				.setName('levelchannel')
-				.setDescription('Set the level channel')
-				.addChannelOption((option) => option.setName('channel').setDescription('The channel to set the level channel to').setRequired(true))
+				.setDescription('Configure the level channel')
+				.addSubcommand((subCommand) =>
+					subCommand
+						.setName('setchannel')
+						.setDescription('Set the level channel')
+						.addChannelOption((option) => option.setName('channel').setDescription('The channel to set the level channel to').setRequired(true))
+				)
+				.addSubcommand((subCommand) => subCommand.setName('removechannel').setDescription('Remove the level channel'))
 		),
+
 	options: {
 		devOnly: true,
 		disabled: false,
 	},
 	async execute(client, interaction, settings) {
 		// Get subcommand
+		const subGroup = interaction.options.getSubcommandGroup();
 		const subCommand = interaction.options.getSubcommand();
 
 		// Defer, Things take time.
 		await interaction.deferReply();
 
-		// Switch subcommand
-		switch (subCommand) {
+		// switch subGroup
+		switch (subGroup) {
 			// Audit log
 			case 'auditlog':
-				// Get channel
-				const auditLogChannel = interaction.options.getChannel('channel');
-				// Make sure channel is a text channel
-				if (!auditLogChannel.isTextBased()) return interaction.followUp('Channel must be a text channel');
-				// Set audit log channel
-				await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { auditLogId: auditLogChannel.id });
-				// Follow up
-				interaction.followUp(`Audit log channel set to ${auditLogChannel}`);
+				if (subCommand === 'setchannel') {
+					// Get channel
+					const auditLogChannel = interaction.options.getChannel('channel');
+					// Make sure channel is a text channel
+					if (!auditLogChannel.isTextBased()) return interaction.followUp('Channel must be a text channel');
+					// Set audit log channel
+					await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { auditLogId: auditLogChannel.id });
+					// Follow up
+					interaction.followUp(`Audit log channel set to ${auditLogChannel}`);
+				} else if (subCommand === 'removechannel') {
+					// Remove audit log channel
+					await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { auditLogId: null });
+					// Follow up
+					interaction.followUp('Audit log channel removed');
+				}
 				break;
 			// Mod log
 			case 'modlog':
-				// Get channel
-				const modLogChannel = interaction.options.getChannel('channel');
-				// Make sure channel is a text channel
-				if (!modLogChannel.isTextBased()) return interaction.followUp('Channel must be a text channel');
-				// Set mod log channel
-				await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { modLogId: modLogChannel.id });
-				// Follow up
-				interaction.followUp(`Mod log channel set to ${modLogChannel}`);
+				if (subCommand === 'setchannel') {
+					// Get channel
+					const modLogChannel = interaction.options.getChannel('channel');
+					// Make sure channel is a text channel
+					if (!modLogChannel.isTextBased()) return interaction.followUp('Channel must be a text channel');
+					// Set mod log channel
+					await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { modLogId: modLogChannel.id });
+					// Follow up
+					interaction.followUp(`Mod log channel set to ${modLogChannel}`);
+				} else if (subCommand === 'removechannel') {
+					// Remove mod log channel
+					await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { modLogId: null });
+					// Follow up
+					interaction.followUp('Mod log channel removed');
+				}
 				break;
 			// Level channel
 			case 'levelchannel':
-				// Get channel
-				const levelChannel = interaction.options.getChannel('channel');
-				// Make sure channel is a text channel
-				if (!levelChannel.isTextBased()) return interaction.followUp('Channel must be a text channel');
-				// Set level channel
-				await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { levelChannelId: levelChannel.id });
-				// Follow up
-				interaction.followUp(`Level channel set to ${levelChannel}`);
+				if (subCommand === 'setchannel') {
+					// Get channel
+					const levelChannel = interaction.options.getChannel('channel');
+					// Make sure channel is a text channel
+					if (!levelChannel.isTextBased()) return interaction.followUp('Channel must be a text channel');
+					// Set level channel
+					await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { levelChannelId: levelChannel.id });
+					// Follow up
+					interaction.followUp(`Level channel set to ${levelChannel}`);
+				} else if (subCommand === 'removechannel') {
+					// Remove level channel
+					await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { levelChannelId: null });
+					// Follow up
+					interaction.followUp('Level channel removed');
+				}
 				break;
 		}
 	},
