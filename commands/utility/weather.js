34d892f19b9a492b4cebb01c13cd52978105007e
MsVoxxie <odysseylove222@gmail.com>
2023-05-14 02:47:19 +0000

    Weather updates

--- commands/utility/weather.js
+++ commands/utility/weather.js
@@ -7,56 +7,71 @@ module.exports = {
 		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
 		.addStringOption((option) => option.setName('location').setDescription('Use Long Location Format').setRequired(true)),
 	options: {
-		devOnly: false,
+		devOnly: true,
 		disabled: false,
 	},
 	async execute(client, interaction, settings) {
+		// API Key
+		const apiKey = process.env.WEATHER_API;
+
 		// Get the locale
-		let locale = interaction.options.getString('location');
-		locale = locale.toLowerCase();
-		locale = locale.charAt(0).toUpperCase() + locale.slice(1);
+		const locale = interaction.options.getString('location');
 
 		// Fetch the weather data
-		const weatherData = await fetch(`https://wttr.in/${locale}?format=j1`).then((response) => {
-			return response.json();
-		});
-		if (!weatherData) return interaction.reply({ content: 'Unable to fetch weather data.', ephemeral: true });
-		const currentConditions = weatherData.current_condition[0];
+		const weatherData = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locale}&aqi=no`).then((response) =>
+			response.json()
+		);
+		if (weatherData.error) return interaction.reply({ content: 'Invalid location provided.', ephemeral: true });
 
 		// Create the embed
 		const embed = new EmbedBuilder()
 			.setImage('https://vii.voxxie.me/v1/client/static/util/divider.png')
-			.setTitle(`Current Weather in ${locale}`)
+			.setTitle(`Current Weather in ${weatherData.location.name}`)
 			.setColor(client.colors.vii)
 			.addFields(
 				{
 					name: 'Temperature',
-					value: `${currentConditions.temp_F}°F / ${currentConditions.temp_C}°C`,
+					value: `${weatherData.current.temp_f}°F / ${weatherData.current.temp_c}°C`,
 					inline: true,
 				},
 				{
 					name: 'Feels Like',
-					value: `${currentConditions.FeelsLikeF}°F / ${currentConditions.FeelsLikeC}°C`,
+					value: `${weatherData.current.feelslike_f}°F / ${weatherData.current.feelslike_c}°C`,
 					inline: true,
 				},
 				{
 					name: 'Humidity',
-					value: `${currentConditions.humidity}%`,
-					inline: true,
-				},
-				{
-					name: 'Precipitation',
-					value: `${currentConditions.precipInches}in / ${currentConditions.precipMM}mm`,
+					value: `${weatherData.current.humidity}%`,
 					inline: true,
 				},
 				{
 					name: 'Wind',
-					value: `${currentConditions.windspeedMiles}mph / ${currentConditions.windspeedKmph}kph`,
+					value: `${weatherData.current.wind_mph} MPH / ${weatherData.current.wind_kph} KPH`,
 					inline: true,
 				},
 				{
-					name: 'Wind Direction',
-					value: `${currentConditions.winddir16Point}`,
+					name: 'UV Index',
+					value: `${weatherData.current.uv}`,
+					inline: true,
+				},
+				{
+					name: 'Cloud Coverage',
+					value: `${weatherData.current.cloud}%`,
+					inline: true,
+				},
+				{
+					name: 'Precipitation',
+					value: `${weatherData.current.precip_in} in / ${weatherData.current.precip_mm} mm`,
+					inline: true,
+				},
+				{
+					name: 'Pressure',
+					value: `${weatherData.current.pressure_in} in / ${weatherData.current.pressure_mb} mb`,
+					inline: true,
+				},
+				{
+					name: 'Visibility',
+					value: `${weatherData.current.vis_miles} mi / ${weatherData.current.vis_km} km`,
 					inline: true,
 				}
 			);
