cd23c09ee5f33be90e5e26c6c42cd089dedcf028
MsVoxxie <odysseylove222@gmail.com>
2023-09-10 15:32:30 -0400

    wake up vii

---
+++ events/executors/04 messageCreate.js
@@ -0,0 +1,25 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.MessageCreate,
+	runType: 'infinity',
+	async execute(client, msg) {
+		const mentionPrefix = `<@${client.user.id}>`;
+		if (!msg.content.toLowerCase().startsWith(mentionPrefix)) return;
+		const args = msg.content.slice(mentionPrefix.length).trim().split(/ +/);
+		const command = args.shift().toLowerCase();
+
+		// Wake up
+		if (command === 'wake') {
+			const wsPing = `\`${Math.round(client.ws.ping)}ms\``;
+			let replies = [
+				`Uwaah! I'm awake!\nDoing my best with ${wsPing} response times!`,
+				`${client.user.username}, Reporting for Duty!\nOperating within ${wsPing} response times!`,
+				`I'm here, I'm here!\nMy response time is ${wsPing}!`,
+				`Sorry ${msg.member.displayName}, I'm here now!\nResponse time is ${wsPing}!`,
+			];
+			const randReply = replies[Math.floor(Math.random() * replies.length)];
+			msg.reply(randReply);
+		}
+	},
+};
