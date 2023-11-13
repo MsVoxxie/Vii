105027e06e73c7ebb7e83225782ceb462f4792b6
MsVoxxie <odysseylove222@gmail.com>
2023-11-12 20:37:10 -0500

    reddyit

---
+++ events/linkFix/redditFix.js
@@ -0,0 +1,18 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.MessageCreate,
+	runType: 'infinity',
+	async execute(client, message) {
+		// Checks
+		if (message.author.bot) return;
+		if (!message.content.startsWith('https://www.reddit.com/')) return;
+
+		//Create new URL
+		const fixedURL = new URL(message);
+		fixedURL.hostname = 'www.rxddit.com';
+
+		// Send Fix
+		await message.reply({ content: fixedURL.href, allowedMentions: { repliedUser: false } });
+	},
+};
