b139758ca1af883bc587cd55409fa12e3e9add12
MsVoxxie <odysseylove222@gmail.com>
2024-04-15 06:05:58 -0400

    better ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, blockQuote, codeBlock } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!').setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
@@ -10,6 +10,19 @@ module.exports = {
 		// Defer, Things take time.
 		await interaction.deferReply();
 
-		await interaction.followUp(`Pong!\n🏓Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+		// Definitions
+		const websocketPing = `${Math.round(client.ws.ping)}ms`;
+		const discordPing = `${Date.now() - interaction.createdTimestamp}ms`;
+
+		// Embed
+		const embed = new EmbedBuilder()
+			.setColor(client.colors.vii)
+			.addFields(
+				{ name: `<:discord_white:1229370575294566431> Websocket`, value: blockQuote(codeBlock(websocketPing)), inline: true },
+				{ name: '<:theconnectionisgood:1229370579102994472> Host<>Discord', value: blockQuote(codeBlock(discordPing)), inline: true }
+			)
+			.setTimestamp();
+
+		await interaction.followUp({ embeds: [embed] });
 	},
 };
